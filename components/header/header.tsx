f1d950e514e35133114070d68ff88a7bf8414485
Devom Brahmbhatt <Devom.b@yahoo.com>
2024-07-17 12:04:47 -0700

    Filler for Header

---
+++ components/header/header.tsx
@@ -0,0 +1,11 @@
+type person = {
+    name: string;
+    isDoctor: boolean;
+    // add more whenever
+};
+
+const Header_component = (person: person) => {
+    return (
+        <p>filler</p>
+    );
+};
