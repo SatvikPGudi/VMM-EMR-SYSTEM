371e829e09fea203928b384706bf02687cc8fad1
Satvik <satvik.gudipati@gmail.com>
2024-07-19 12:00:51 -0700

    Data is encrypted

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 async function main() {
   const doctor = await prisma.doctor.create({
     data: {
-      name: 'Dr. Tiffany',
+      name: 'Dr.Tiffany',
     },
   });
 
