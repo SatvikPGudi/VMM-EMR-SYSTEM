6021e3b0cfd400c986d19a610aa70b3b2da28663
Devom Brahmbhatt <Devom.b@yahoo.com>
2024-07-27 21:25:08 -0700

    Added JSX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,351 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <h3>
+        Date of clinic visit:
+        <br />
+        <br />
+        Patient name:
+        <br />
+        <br />
+        Date of birth:
+      </h3>
+
+      <form className="alignment" action="/submit" method="POST">
+        <hr />
+
+        <div>
+          <h1 className="title">Subjective</h1>
+
+          <h2 className="pt-4">
+            <label htmlFor="chief-complaint">Chief Complaint</label>
+          </h2>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="chief-complaint"
+            name="chief-complaint"
+          ></textarea>
+
+          <h2 className="pt-4">
+            <label htmlFor="hpi">HPI</label>
+          </h2>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="hpi"
+            name="hpi"
+          ></textarea>
+
+          <h2 className="pt-8">History:</h2>
+
+          <h4 className="pt-2">
+            <label htmlFor="medical-history">Past Medical History</label>
+          </h4>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="medical-history"
+            name="medical-history"
+          ></textarea>
+
+          <h4 className="pt-2">
+            <label htmlFor="surgical-history">Surgical History</label>
+          </h4>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="surgical-history"
+            name="surgical-history"
+          ></textarea>
+
+          <h4 className="pt-2">
+            <label htmlFor="family-history">Family History</label>
+          </h4>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="family-history"
+            name="family-history"
+          ></textarea>
+
+          <h4 className="pt-2">
+            <label htmlFor="social-history">Social History</label>
+          </h4>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="social-history"
+            name="social-history"
+          ></textarea>
+
+          <h2 className="pt-8">
+            <label htmlFor="allergies">Allergies</label>
+          </h2>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="allergies"
+            name="allergies"
+          ></textarea>
+
+          <h2 className="pt-4">
+            <label htmlFor="medications">Medications</label>
+          </h2>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="medications"
+            name="medications"
+          ></textarea>
+
+          <h2 className="pt-4" id="ROS">
+            <label htmlFor="symptom-review">Review of Systems</label>
+          </h2>
+          <textarea
+            className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+            rows={3}
+              cols={60}
+            id="symptom-review"
+            name="symptom-review"
+          ></textarea>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <hr />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div>
+          <h1 className="title pt-12">Objective</h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <h2 className="pt-4">Vital Signs:</h2>
+          <div>
+            <div className="pt-2">
+              <label htmlFor="height" className="label">Height:</label>
+              <input
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                type="text"
+                id="height"
+                name="height"
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="weight" className="label">Weight:</label>
+              <input
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                type="text"
+                id="weight"
+                name="weight"
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="bmi" className="label">BMI:</label>
+              <input
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                type="text"
+                id="bmi"
+                name="bmi"
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="blood-pressure" className="label">Blood Pressure:</label>
+              <input
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                type="text"
+                id="blood-pressure"
+                name="blood-pressure"
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="temp" className="label">Temperature:</label>
+              <input
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                type="text"
+                id="temp"
+                name="temp"
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="pulse" className="label">Pulse:</label>
+              <input
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                type="text"
+                id="pulse"
+                name="pulse"
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="oxygen" className="label">O2 Oximeter:</label>
+              <input
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                type="text"
+                id="oxygen"
+                name="oxygen"
+              />
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <br />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div>
+            <h2 className="pt-4">Physical Exam:</h2>
+            <div className="pt-2">
+              <h4><label>Constitutional</label></h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+               cols={60}
+                id="constitutional"
+              ></textarea>
+            </div>
+            <div className="pt-2">
+              <h4>
+                <label htmlFor="ent">ENT</label>
+              </h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+               cols={60}
+                id="ent"
+                name="ent"
+              ></textarea>
+            </div>
+            <div className="pt-2">
+              <h4>
+                <label htmlFor="respiratory">Respiratory</label>
+              </h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+               cols={60}
+                id="respiratory"
+                name="respiratory"
+              ></textarea>
+            </div>
+            <div className="pt-2">
+              <h4>
+                <label htmlFor="cardio">Cardio</label>
+              </h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+                cols={60}
+                id="cardio"
+                name="cardio"
+              ></textarea>
+            </div>
+            <div className="pt-2">
+              <h4>
+                <label htmlFor="cal">Cal</label>
+              </h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+                cols={60}
+                id="cal"
+                name="cal"
+              ></textarea>
+            </div>
+            <div className="pt-2">
+              <h4>
+                <label htmlFor="skin">Skin</label>
+              </h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+                cols={60}
+                id="skin"
+                name="skin"
+              ></textarea>
+            </div>
+            <div className="pt-2">
+              <h4>
+                <label htmlFor="extremities">Extremities</label>
+              </h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+                cols={60}
+                id="extremities"
+                name="extremities"
+              ></textarea>
+            </div>
+            <div className="pt-2">
+              <h4>
+                <label htmlFor="neuro">Neuro</label>
+              </h4>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={3}
+                cols={60}
+                id="neuro"
+                name="neuro"
+              ></textarea>
+            </div>
+            <div className="pt-4">
+              <h2>
+                <label htmlFor="test-results">Test Results</label>
+              </h2>
+              <input className="pl-20" type="file" id="test-results" name="test-results" />
+            </div>
+          </div>
+          <br />
+
+          <div className="pt-4">
+            <div>
+              <h1 className="title">
+                <label htmlFor="assessment">Assessment</label>
+              </h1>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={20}
+                cols={110}
+                id="assessment"
+                name="assessment"
+              ></textarea>
+            </div>
+            <br />
+            <div>
+              <h1 className="title">
+                <label htmlFor="plan">Plan</label>
+              </h1>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={20}
+                cols={110}
+                id="plan"
+                name="plan"
+              ></textarea>
+            </div>
+            <br />
+            <div>
+              <h1 className="title">
+                <label htmlFor="follow-up">Follow Up</label>
+              </h1>
+              <textarea
+                className="transition-all duration-500 rounded-md border-2 border-gray-300 hover:border-black"
+                rows={20}
+                cols={110}
+                id="follow-up"
+                name="follow-up"
+              ></textarea>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
